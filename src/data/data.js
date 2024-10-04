245cb84a736641f554ddb6d7e2d920761d2658bc
relkassm <57866875+relkassm@users.noreply.github.com>
2024-10-04 14:33:10 +0100

    added: im-data

--- src/data/data.js
+++ src/data/data.js
@@ -18,7 +18,7 @@ const data = [
         index: "3",
         key: "Career",
         value:
-          "My professional journey began as a Freelance Web Developer where I built a portfolio of responsive websites and applications for a diverse clientele. Each project presented its own set of challenges, from gathering requirements to developing custom solutions and managing everything from design to deployment. Working independently, I was responsible for ensuring that every project was delivered on time and within budget, which helped me develop strong project management skills.<br /><br />In 2020, I joined IM Creation as a Web Developer, where I took on a leadership role in driving the technology strategy to meet clients' goals. My responsibilities went beyond coding; I managed a multidisciplinary team of developers, designers, and tech professionals, ensuring that each project was executed seamlessly from concept to completion. I also introduced quality control processes that improved the overall standard of our web projects. This position allowed me to grow both as a leader and as a technical expert, as I was responsible for overseeing all aspects of project delivery.<br /><br />In September 2023, I embraced a new challenge at Ark-x Talent Factory as an Instructor and Software Developer. Here, I mentor aspiring developers, sharing my knowledge of programming and engineering to help them grow into skilled professionals. Beyond teaching, I am also responsible for creating engaging curriculum content tailored for our learning management system. My role includes designing custom software solutions to optimize the company's internal operations, ensuring that our team stays on track with project deliverables and making improvements where necessary. This experience has been incredibly rewarding, as I get to shape the next generation of developers while continuing to push the boundaries of my own capabilities.",
+          "My professional journey began as a Freelance Web Developer where I built a portfolio of responsive websites and applications for a diverse clientele. Each project presented its own set of challenges, from gathering requirements to developing custom solutions and managing everything from design to deployment. Working independently, I was responsible for ensuring that every project was delivered on time and within budget, which helped me develop strong project management skills.<br /><br />In 2020, I co-founded IM Creation as a technical director where I took on a leadership role in driving the technology strategy to meet clients' goals. My responsibilities went beyond coding; I managed a multidisciplinary team of designers, and tech professionals, ensuring that each project was executed seamlessly from concept to completion. I also introduced quality control processes that improved the overall standard of our web projects. This position allowed me to grow both as a leader and as a technical expert, as I was responsible for overseeing all aspects of project delivery.<br /><br />In September 2023, I embraced a new challenge at Ark-x Talent Factory as an Instructor and Software Developer. Here, I mentor aspiring developers, sharing my knowledge of programming and engineering to help them grow into skilled professionals. Beyond teaching, I am also responsible for creating engaging curriculum content tailored for our learning management system. My role includes designing custom software solutions to optimize the company's internal operations, ensuring that our team stays on track with project deliverables and making improvements where necessary. This experience has been incredibly rewarding, as I get to shape the next generation of developers while continuing to push the boundaries of my own capabilities.",
       },
     ],
   },
@@ -73,19 +73,35 @@ const data = [
         index: "1",
         key: "Company",
         value:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime sed illo facere pariatur dicta, enim architecto magnam animi sit voluptates doloremque molestiae rerum. Aliquid, sed at. Et consequuntur quia ducimus.",
+          "IM Creation, based in Marrakech, is a specialized agency focused on creating cutting-edge digital content and tailored solutions for businesses. With a strong foundation in web development and digital marketing, the agency leverages its technical expertise to deliver high-quality websites, applications, and content strategies that drive engagement.",
       },
       {
         index: "2",
         key: "Role",
         value:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime sed illo facere pariatur dicta, enim architecto magnam animi sit voluptates doloremque molestiae rerum. Aliquid, sed at. Et consequuntur quia ducimus.",
+          "As the co-founder and technical director of IM-Creation, I played a pivotal role in shaping the agency's technology strategy to meet the diverse needs of our clients. My responsibilities extended far beyond coding; I led a multidisciplinary team of designers and technical professionals, ensuring each project was delivered efficiently and met the highest standards. By introducing robust quality control processes, I enhanced the consistency and performance of our web projects. This leadership role allowed me to grow both as a strategic thinker and a technical expert, overseeing the entire project lifecycle from concept to completion.",
       },
       {
         index: "3",
         key: "Projects",
-        value:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime sed illo facere pariatur dicta, enim architecto magnam animi sit voluptates doloremque molestiae rerum. Aliquid, sed at. Et consequuntur quia ducimus.",
+        value: "Use arrows to navigate between projects",
+        data: [
+          {
+            title: "IM Creation",
+            text: "The Official website of the agency.",
+            image: "/data/imcreation.gif",
+          },
+          {
+            title: "IM Reporting",
+            text: "A web application used to share reporting from press and social media listening with active clients.",
+            image: "/data/imreporting.png",
+          },
+          {
+            title: "IM Solution",
+            text: "A web application for social listening with an advanced UX and features including AI data processing.",
+            image: "/data/imsolution.png",
+          },
+        ],
       },
     ],
   },
